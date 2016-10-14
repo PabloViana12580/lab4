7953864e35b80fbbae7a3adb89a2d48bca6d23d6
JoséPablo <JosPablo@172.20.5.147>
2016-10-14 09:08:59 -0600

    Solucion ejercicio 3

--- src/AlarmaEmergencia.java
+++ src/AlarmaEmergencia.java
@@ -1,5 +1,5 @@
 
-public class AlarmaEmergencia {
+public class AlarmaEmergencia extends Alarma {
 
 	public AlarmaEmergencia() {
 }
@@ -8,6 +8,14 @@ public class AlarmaEmergencia {
 		System.out.println("Emergencia 1");
 	}	
 	
-	public void m2()
+	public void m2(){
+		super.m1();
+		super.m2();
+	}
+	
+	public String toString(){
+		String cadena = "Emergencia "+ super.toString();
+		return cadena;
+	}
 
 }
